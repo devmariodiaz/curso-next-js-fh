882d265f2abb4fc6e224260682720173765715d1
Mario Diaz <dev.mariodiaz@gmail.com>
2024-03-20 22:46:37 +0100

    43. pensemos en hojas y pequeños componentes

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,8 +1,19 @@
+import { CartCounter } from "@/app/shopping-cart";
+
+
+export const metadata = {
+    title: "Counter",
+    description: "Counter page"
+    
+}
 
 export default function CounterPage() {
+
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito de compras</span>
+      <CartCounter value={ 10 } />
     </div>
   );
 }
+
