426536c0c9bc107b19ba13e1eb4a296522cc04d5
user.name  Mario Diaz <user.email dev.mariodiaz@gmail.com>
2024-03-27 20:40:33 +0100

    54. Image Priority - Propiedad de Carga

---
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,26 @@
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from 'next/image';
+import { PokemonGrid } from '../../pokemons/components/PokemonGrid';
+
+const getPokemons = async (limit = 151, offset = 0): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+  .then((res) => res.json());
+
+  const pokemons = data.results.map( pokemon => ({
+    id: pokemon.url.split('/').at(-2)!,
+    name: pokemon.name
+  }));
+
+  return pokemons;
+};
+
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
+  return (
+    <div className="flex flex-col">
+      <span className="text-5xl my-2">Listado de Pókemons <small>estático</small></span>
+      <PokemonGrid pokemons={ pokemons } />
+    </div>
+  );
+}
---
+++ 02-my-dashboard/src/app/pokemons/components/PokemonCard.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import { SimplePokemon } from ".."
+import Link from "next/link";
+import { IoHeart, IoHeartOutline } from "react-icons/io5";
+
+interface Props {
+    pokemon: SimplePokemon;
+}
+
+export const PokemonCard = ({ pokemon } : Props) => {
+
+    const { id, name } = pokemon;
+
+  return (
+    <div className="mx-auto right-0 mt-2 w-60">
+        <div className="bg-white rounded overflow-hidden shadow-lg">
+            <div className="flex flex-col items-center justify-center text-center p-6 bg-gray-800 border-b">
+            <Image
+                className="h-32 w-32 text-white rounded-full mx-auto"
+                key={ id }
+                src={ `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ id }.png` }
+                width={ 100 }
+                height={ 100 }
+                alt={ name }
+                priority={ false }
+            />
+            <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">{ pokemon.name }</p>
+            <p className="text-sm text-gray-100">John@Doe.com</p>
+            <div className="mt-5">
+                <Link
+                 href={`/pokemon/${id}`}
+                 className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100">
+                    Más información
+                </Link>
+            </div>
+            </div>
+            <div className="border-b">
+                <Link href="/dashboard/main" className="px-4 py-2 hover:bg-gray-100 flex items-center">
+                    <div className="text-green-600">
+                        <IoHeartOutline className="text-red-600" />
+                    </div>
+                    <div className="pl-3">
+                    <p className="text-sm font-medium text-gray-800 leading-none">
+                        No es favorito
+                    </p>
+                    <p className="text-xs text-gray-500">View your campaigns</p>
+                    </div>
+                </Link>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
